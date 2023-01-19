7c6b3f0d3b6f540c1727b1acdc6d7e02a3c7fac3
Eric Knorr <eric_knorr@g.harvard.edu>
2023-01-19 16:04:33 -0500

    changed number of threads to powers of 2

--- SSD_modelling/pdam_test.sh
+++ SSD_modelling/pdam_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 RW_FLAG=$1
-THREADS=$2
-RW_SIZE=$3
-FILE_SIZE=$4
+RW_SIZE=$2
+FILE_SIZE=$3
+ROUNDS=$4
 OP="read"
 
 if [ $RW_FLAG = "w" ]
@@ -15,12 +15,22 @@ rm ${OP}.csv
 make ${OP}
 echo -ne " [STATUS] gernerating ${FILE_SIZE}GB file...\r"
 head -c ${FILE_SIZE}G </dev/urandom >file
-for (( k = 1; k <= $THREADS; ++k)); do
-    echo -ne " [STATUS] ${OP} $(( k*10 ))GB with ${k} threads...\033[0K\r"
-    sync; echo 3 > /proc/sys/vm/drop_caches
-    ./${OP} ${THREADS} ${RW_SIZE} >> ${OP}.csv
-    echo -n ", " >> ${OP}.csv
-    echo " [STATUS] ${OP} $(( k*RW_SIZE ))GB with ${k} threads - DONE."
+# for (( k = 1; k <= $THREADS; ++k)); do
+for i in {1..${ROUNDS}}; do
+    for k in 1 2 4 8 16 32 64; do 
+        echo -ne " [STATUS] ${OP} $(( k*10)) with ${k} threads...\033[0K\r"
+        sync; echo 3 > /proc/sys/vm/drop_caches
+        ./${OP} ${k} ${RW_SIZE} >> ${OP}.csv
+        if [${k} -eq ${ROUNDS}]
+        then
+            echo -n ", " >> ${OP}.csv
+        else 
+            echo -ne "\n" >> ${OP}.csv
+        fi
+
+        echo " [STATUS] ${OP} $(( k*10 ))GB with ${k} threads - DONE."
+    done
+
 done
 
 echo "Benchmark complete, results written to ${OP}.csv"
