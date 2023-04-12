b5b224e21ff2b4af84d8f5945854186dbe4c44ae
Eric Knorr <eric_knorr@g.harvard.edu>
2023-04-12 13:36:46 -0400

    renamed some stuff.  combined benchmarks into one.  full support for random/sequential + read/write

---
+++ mico_benchmarks/micro_bench.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/random.h>
+
+static size_t file_size = (size_t) 16*1024*1024*1024;
+static size_t access_size = 1024*64;
+static size_t num_accesses = 163840;
+static char * to_write = NULL;
+static unsigned short read_flag = 0;
+static unsigned short rand_flag = 0;
+
+void * worker_thread(void * arg) 
+{
+    int ret = 0;
+    char buffer[access_size];
+    size_t * offsets = (size_t *) arg;
+    int fd = -1;
+    
+    if (rand_flag)
+    {
+        if (read_flag)
+        {
+            fd = open("file", O_RDONLY);
+        } 
+        else 
+        {
+            fd = open("file", O_WRONLY);
+        }
+        if (fd == -1)
+        {
+            fprintf(stderr, "Failed to open data file: %s\n", strerror(errno));
+            exit(1);
+        }
+        for (size_t i = 0; i < num_accesses; i++)
+        {
+            if (read_flag) 
+            {
+
+                ret = pread(fd, &buffer, access_size, offsets[i]);
+            }
+            else
+            {
+                ret = pwrite(fd, to_write, access_size, offsets[i]);
+            }
+            if (ret < access_size)
+            {
+                fprintf(stderr, "ERROR: only %d B of %d B read/written: %s", ret, access_size, strerror(errno));
+            }
+            
+        }
+    }
+    else
+    {
+        char file_name[128];
+        sprintf(file_name, "thread_%u", pthread_self());
+        if (read_flag)
+        {
+            fd = open("file", O_RDONLY);
+        } 
+        else 
+        {
+            fd = open(file_name, O_WRONLY);
+        }
+        if (fd == -1)
+        {
+            fprintf(stderr, "Failed to open data file: %s\n", strerror(errno));
+            exit(1);
+        }
+        for (size_t i = 0; i < num_accesses; i++)
+        {
+            if (read_flag) 
+            {
+                pread(fd, &buffer, access_size, offsets[0] + i*access_size);
+            }
+            else
+            {
+                pwrite(fd, to_write, access_size, i*access_size);
+            }
+        }
+    }
+    close(fd);
+}
+
+
+int main(int argc, char ** argv)
+{
+    // parse arguments, <num_threads> <access_size(KB)> <num_accesses> <file_size(GB)> <r/w> <r/s>
+    int num_threads = atoi(argv[1]);
+    access_size = 1024 * (size_t) atoi(argv[2]);
+    num_accesses = (size_t) atoi(argv[3]);
+    file_size = 1024 * 1024 * 1024 * (size_t) atoi(argv[4]);
+    if (strcmp(argv[5], "r") == 0) 
+    {
+        read_flag = 1;
+    }
+    if (strcmp(argv[6], "r") == 0)
+    {
+        rand_flag = 1;
+    }
+    pthread_t threads[num_threads];
+    size_t * offsets = (size_t *) malloc(sizeof(size_t)*num_threads*num_accesses);
+    if (rand_flag) 
+    {
+        size_t ret = getrandom((void*)offsets, sizeof(size_t)*num_threads*num_accesses, 0);
+        if (ret == (size_t)-1 || ret < sizeof(size_t)*num_threads*num_accesses)
+        {
+            fprintf(stderr, "Failed to gernerate random offsets: %s\n", strerror(errno));
+            exit(1);
+        }
+        for (size_t i = num_threads*num_accesses; i > 0; i--)
+        {
+            offsets[i] = offsets[i] % (size_t)(file_size - access_size);
+        }
+    }
+    else 
+    {
+        for (size_t i = 0; i < num_threads; i++)
+        {
+            offsets[i*num_accesses] = i * (file_size - access_size)/ num_threads;
+        }
+    }
+
+    if (!read_flag) {
+        to_write = (char*)malloc(access_size);
+        int fd = open("/dev/urandom", O_RDONLY);
+        if (fd == -1)
+        {
+            fprintf(stderr, "Failed to open /dev/urandom: %s\n", strerror(errno));
+            exit(1);
+        }
+        read(fd, to_write, access_size);
+    }
+
+    struct timeval start, stop;
+    gettimeofday(&start, NULL);
+
+    for (int i = 0; i < num_threads; i++)
+    {
+        pthread_create(&threads[i], NULL, worker_thread, (void*) (offsets + i * num_accesses));
+    }
+    for (int i = 0; i < num_threads; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+
+    gettimeofday(&stop, NULL);
+
+    fprintf(stdout, "%lu", (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec);
+    return 0;
+}
+
---
+++ mico_benchmarks/run.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# "r" for read "w" for write
+RW_FLAG=$1
+# "r" for random "s" for sequential access
+RS_FLAG=$2
+# size to be read/written each time (KiB)
+RW_SIZE=$3
+# number of accesses
+NUM_ACCESS=$4
+# size of the file read/written from/to (GiB)
+FILE_SIZE=$5
+# max number of threads to run
+MAX_THREAS=$6
+# number of rounds to repeat the experiment
+ROUNDS=$7
+OP="read"
+OR="rand"
+
+if [ $RW_FLAG = "w" ]
+then
+    OP="write"
+fi
+if [ $RS_FLAG = "s" ]
+then
+    OR="seq"
+    for (( i=1; i <= ))
+fi
+
+# rm ${OP}.csv
+make micro_bench
+echo -ne " [STATUS] gernerating ${FILE_SIZE}GB file...\r"
+if [ $RS_FLAG ]
+head -c ${FILE_SIZE}G </dev/urandom >file
+for (( i = 1; i <= $ROUNDS; i++)); do
+    echo "${i}"
+    # for k in 1 2 4 8 16 32 64; do
+    for (( k=1; k <= MAX_THREADS; k = k * 2 )); do
+        echo -ne " [STATUS] ${OP}_${OR} $(( k*RW_SIZE*NUM_ACCESS ))B with ${k} threads...\033[0K\r"
+        sync; echo 3 > /proc/sys/vm/drop_caches
+        ./micro_bench ${k} ${RW_SIZE} ${NUM_ACCESS} ${FILE_SIZE} ${RW_FLAG} ${RS_FLAG} >> ${OP}_${OR}.csv
+        if [ ${k} -ne 64 ]
+        then
+            echo -n ", " >> ${OP}_${OR}.csv
+        else 
+            echo -ne "\n" >> ${OP}_${OR}.csv
+        fi
+
+        echo " [STATUS] ${OP}_${OR} $(( k*RW_SIZE*NUM_ACCESS ))B with ${k} threads - DONE."
+    done
+done
+
+echo "Benchmark complete, results written to ${OP}_${OR}.csv"
