efa01be72d8a431d11997ada9ff128203ba9e229
Erin Ransom <erins.ransom@gmail.com>
2023-12-13 17:35:43 +0000

    removed obsolete code

--- micro_benchmarks/micro_bench.c
+++ micro_benchmarks/micro_bench.c
@@ -13,7 +13,7 @@ static size_t access_size = 1024*64;
 static size_t num_accesses = 163840;
 static char * to_write = NULL;
 static unsigned short read_flag = 0;
-static unsigned short rand_flag = 0;
+static char * working_file = NULL;
 
 void * worker_thread(void * arg) 
 {
@@ -22,67 +22,35 @@ void * worker_thread(void * arg)
     size_t * offsets = (size_t *) arg;
     int fd = -1;
     
-    if (rand_flag)
+    if (read_flag)
     {
-        if (read_flag)
-        {
-            fd = open("file", O_RDONLY);
-        } 
-        else 
-        {
-            fd = open("file", O_WRONLY);
-        }
-        if (fd == -1)
-        {
-            fprintf(stderr, "Failed to open data file: %s\n", strerror(errno));
-            exit(1);
-        }
-        for (size_t i = 0; i < num_accesses; i++)
-        {
-            if (read_flag) 
-            {
-
-                ret = pread(fd, &buffer, access_size, offsets[i]);
-            }
-            else
-            {
-                ret = pwrite(fd, to_write, access_size, offsets[i]);
-            }
-            if (ret < access_size)
-            {
-                fprintf(stderr, "ERROR: only %d B of %lu B read/written: %s\n", ret, access_size, strerror(errno));
-            }
-            
-        }
+        fd = open(working_file, O_RDONLY);
+    } 
+    else 
+    {
+        fd = open(working_file, O_WRONLY);
     }
-    else
+    if (fd == -1)
     {
-        char file_name[128];
-        sprintf(file_name, "thread_%lu", pthread_self());
-        if (read_flag)
+        fprintf(stderr, "Failed to open data file: %s\n", strerror(errno));
+        exit(1);
+    }
+    for (size_t i = 0; i < num_accesses; i++)
+    {
+        if (read_flag) 
         {
-            fd = open("file", O_RDONLY);
-        } 
-        else 
-        {
-            fd = open(file_name, O_WRONLY | O_CREAT);
+
+            ret = pread(fd, &buffer, access_size, offsets[i]);
         }
-        if (fd == -1)
+        else
         {
-            fprintf(stderr, "Failed to open data file: %s\n", strerror(errno));
-            exit(1);
+            ret = pwrite(fd, to_write, access_size, offsets[i]);
         }
-        for (size_t i = 0; i < num_accesses; i++)
+        if (ret < access_size)
         {
-            if (read_flag) 
-            {
-                pread(fd, &buffer, access_size, offsets[0] + i*access_size);
-            }
-            else
-            {
-                pwrite(fd, to_write, access_size, i*access_size);
-            }
+            fprintf(stderr, "ERROR: only %d B of %lu B read/written: %s\n", ret, access_size, strerror(errno));
         }
+        
     }
     close(fd);
 }
@@ -90,41 +58,29 @@ void * worker_thread(void * arg)
 
 int main(int argc, char ** argv)
 {
-    // parse arguments: <num_threads> <access_size(KB)> <file_size(MB)> <r/w> <r/s>
-    int num_threads = atoi(argv[1]);
-    access_size = 1024 * (size_t) atoi(argv[2]);
+    // parse arguments: <file> <num_threads> <access_size(KB)> <file_size(MB)> <r/w> <r/s>
+    working_file = argv[1];
+    int num_threads = atoi(argv[2]);
+    access_size = 1024 * (size_t) atoi(argv[3]);
     // num_accesses = (size_t) atoi(argv[3]);
     num_accesses = (size_t) 10 * 1024 * 1024 * 1024 / access_size; 
-    file_size = 1024 * 1024 * (size_t) atoi(argv[3]);
-    if (strcmp(argv[4], "r") == 0) 
+    file_size = 1024 * 1024 * (size_t) atoi(argv[4]);
+    if (strcmp(argv[5], "r") == 0) 
     {
         read_flag = 1;
     }
-    if (strcmp(argv[5], "r") == 0)
-    {
-        rand_flag = 1;
-    }
+
     pthread_t threads[num_threads];
     size_t * offsets = (size_t *) malloc(sizeof(size_t)*num_threads*num_accesses);
-    if (rand_flag) 
+    size_t ret = getrandom((void*)offsets, sizeof(size_t)*num_threads*num_accesses, 0);
+    if (ret == (size_t)-1 || ret < sizeof(size_t)*num_threads*num_accesses)
     {
-        size_t ret = getrandom((void*)offsets, sizeof(size_t)*num_threads*num_accesses, 0);
-        if (ret == (size_t)-1 || ret < sizeof(size_t)*num_threads*num_accesses)
-        {
-            fprintf(stderr, "Failed to gernerate random offsets: %s\n", strerror(errno));
-            exit(1);
-        }
-        for (size_t i = 0; i < num_threads*num_accesses; i++)
-        {
-            offsets[i] = offsets[i] % (size_t)(file_size - access_size);
-        }
+        fprintf(stderr, "Failed to gernerate random offsets: %s\n", strerror(errno));
+        exit(1);
     }
-    else 
+    for (size_t i = 0; i < num_threads*num_accesses; i++)
     {
-        for (size_t i = 0; i < num_threads; i++)
-        {
-            offsets[i*num_accesses] = i * (file_size - access_size*2)/ num_threads;
-        }
+        offsets[i] = offsets[i] % (size_t)(file_size - access_size);
     }
 
     if (!read_flag) {
