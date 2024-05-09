ee5ea05f97a2f78514f7874792e7966955fe942a
Erin Ransom <erins.ransom@gmail.com>
2024-05-09 14:29:49 +0000

    update workload_gen

--- workload_gen.cc
+++ workload_gen.cc
@@ -14,25 +14,21 @@ int main(int argc, char *argv[]) {
 
     assert(std::filesystem::exists(DATA_DIR));
 
-    std::vector<uint64_t> keys;
-    keys.reserve(nkeys);
+    std::stringstream ss;
+    ss << nkeys << ".txt";
+    std::ofstream fout;
+    fout.open(ss.str(), std::ios::out);
 
     std::random_device rd;  // Will be used to obtain a seed for the random number engine
     std::mt19937_64 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
     std::uniform_int_distribution<uint64_t> uni_dist(0, UINT64_MAX);
 
-    while (keys.size() < nkeys) {
+    for (size_t i = 0; i < nkeys; i++) {
         uint64_t number = uni_dist(gen);
-        keys.push_back(number);
+        fout << number << "\n";
     }
 
-    std::stringstream ss;
-    ss << nkeys << ".txt";
-    std::ofstream output_file(ss.str());
-    std::ostream_iterator<uint64_t> output_iterator(output_file, "\n");
-
-    std::copy(keys.begin(), keys.end(), output_iterator);
-    output_file.close();
+    fout.close();
 
     return 0;
 }
