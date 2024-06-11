9e72eee48eb3bf9165f5080d2e71dba254634aa5
Erin Ransom <erins.ransom@gmail.com>
2024-06-11 14:41:32 +0000

    Update workload_gen

--- workload_gen.cc
+++ workload_gen.cc
@@ -7,28 +7,32 @@
 #include <filesystem>
 
 int main(int argc, char *argv[]) {
-    assert(argc == 3);
+    assert(argc == 4);
 
     const std::string DATA_DIR = argv[1];
-    unsigned long long nkeys = strtoull(argv[2], NULL, 0);
+    size_t nkeys = atoi(argv[2]);
+    size_t nthreads = atoi(argv[3]);
+    assert(nthreads > 0);
 
     assert(std::filesystem::exists(DATA_DIR));
-
-    std::stringstream ss;
-    ss << DATA_DIR << nkeys << ".txt";
-    std::ofstream fout;
-    fout.open(ss.str(), std::ios::out);
+    std::string path = DATA_DIR + "/" + std::to_string(nthreads) + "/" + std::to_string(nkeys);
+    std::filesystem::create_directories(path);
 
     std::random_device rd;  // Will be used to obtain a seed for the random number engine
     std::mt19937_64 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
     std::uniform_int_distribution<uint64_t> uni_dist(0, UINT64_MAX);
 
-    for (size_t i = 0; i < nkeys; i++) {
-        uint64_t number = uni_dist(gen);
-        fout << number << "\n";
+    size_t sz = ((nkeys + (nthreads - 1)) / nthreads);
+    size_t total = 0;
+    for (size_t i = 0; i < nthreads; i++) {
+        std::ofstream fout;
+        fout.open(path + "/" + std::to_string(i), std::ios::out);
+        for (size_t j = 0; j < sz && total < nkeys; j++, total++) {
+            uint64_t number = uni_dist(gen);
+            fout << number << "\n";
+        }
+        fout.close();
     }
-
-    fout.close();
 
     return 0;
 }
